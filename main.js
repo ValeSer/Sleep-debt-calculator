aa0d403318e9ae4d0d6959baec2b8f3282649b41
Valeria Serci <balerty84@gmail.com>
2022-10-31 12:50:35 +0000

    refactor switch case

--- main.js
+++ main.js
@@ -2,28 +2,18 @@ const getSleepHours = day => {
     switch(day) {
       case 'sunday':
         return 12;
-        break;
       case 'monday':
         return 5;
-        break;
       case 'tuesday':
-        return 6;
-        break;
       case 'wednesday':
         return 6;
-        break;
       case 'thursday':
-        return 7;
-        break;
       case 'friday':
         return 7;
-        break;
       case 'saturday':
         return 10;
-        break;
-        default:
-      return 'invalid';
-      break;
+      default:
+        return 'invalid';
     }
   };
   
