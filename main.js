f3f853c29d27d14f0ba6ddf3acea852f733fa808
Valeria Serci <balerty84@gmail.com>
2022-10-31 12:02:56 +0000

    implement switch cases for all the week

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const getSleepHours = day => {
     switch(day) {
-        case 'monday':
-        return 8;
+      case 'sunday':
+        return 12;
         break;
-      }
-};
+      case 'monday':
+        return 5;
+        break;
+      case 'tuesday':
+        return 6;
+        break;
+      case 'wednesday':
+        return 6;
+        break;
+      case 'thursday':
+        return 7;
+        break;
+      case 'friday':
+        return 7;
+        break;
+      case 'saturday':
+        return 10;
+        break;
+    }
+  };
+  
 
-console.log(getSleepHours('monday'));
+console.log(getSleepHours('tuesday'));
